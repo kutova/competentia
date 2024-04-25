b7834ee21caf5c9474ffdc76180ae2d64f856697
Marcos Kutova <kutova@gmail.com>
2024-04-25 09:00:26 -0300

    Versão PDF do relatório do currículo
    Criação da versão PDF do relatório dos componentes curriculares e outros ajustes.

--- scripts/relatorio-curriculo.js
+++ scripts/relatorio-curriculo.js
@@ -73,3 +73,110 @@ let mostraTabela = function () {
 
 // Mostra a tabela de competências
 mostraTabela();
+
+btnImprimir.onclick = () => {
+  let doc = new jspdf.jsPDF({
+    orientation: "p",
+    unit: "pt",
+    format: "a4",
+  });
+
+  doc.setFontSize(10);
+  doc.setFont("helvetica", "normal");
+  let totalPagesExp = "{total_pages_count_string}";
+
+  let listaComponentesCurriculares = dbComponentesCurriculares_Cursos
+    .componentesCurriculares(curso.id)
+    .map((componenteCurricular_Curso) => ({
+      ...dbComponentesCurriculares.componenteCurricular(
+        componenteCurricular_Curso.idComponenteCurricular
+      ),
+      semestre: componenteCurricular_Curso.semestre,
+    }))
+    .sort((a, b) =>
+      a.semestre == b.semestre
+        ? a.nome.localeCompare(b.nome)
+        : a.semestre - b.semestre
+    );
+
+  for (sem = 1; sem <= curso.semestres; sem++) {
+    let listaComponentesSemestre = listaComponentesCurriculares.filter(
+      (c) => c.semestre == sem
+    );
+
+    doc.autoTable({
+      head: [
+        [
+          {
+            content: sem + "º Semestre",
+            colSpan: 5,
+          },
+        ],
+        [
+          "Componente Curricular",
+          "CH",
+          "Tipo",
+          "Área",
+          "Componentes de Competência",
+        ],
+      ],
+      body: listaComponentesSemestre.map((c) => [
+        c.nome,
+        c.cargaHoraria,
+        tiposComponentesCurriculares[c.tipo],
+        dbAreas.area(c.area).nome,
+
+        dbComponentesCompetencias_ComponentesCurriculares
+          .componentesCompetencias(c.id)
+          .map((idComponenteCompetencia) =>
+            dbComponentesCompetencias.componenteCompetencias(
+              idComponenteCompetencia
+            )
+          )
+          .sort((a, b) =>
+            a.tipo == b.tipo ? a.nome.localeCompare(b.nome) : a.tipo - b.tipo
+          )
+          .map(
+            (cc) =>
+              cc.nome +
+              " (" +
+              iniciaisTiposComponentesCompetencias[cc.tipo] +
+              ")"
+          )
+          .join(", "),
+      ]),
+      willDrawPage: function (data) {
+        doc.setFontSize(18);
+        doc.text("COMPETENTIA - " + curso.nome, 40, 40);
+      },
+      didDrawPage: function (data) {
+        // Footer
+        var str = "Página " + doc.internal.getNumberOfPages();
+        // Total page number plugin only available in jspdf v1.0+
+        if (typeof doc.putTotalPages === "function") {
+          str = str + " de " + totalPagesExp;
+        }
+        doc.setFontSize(10);
+
+        // jsPDF 1.4+ uses getHeight, <1.4 uses .height
+        var pageSize = doc.internal.pageSize;
+        var pageHeight = pageSize.height
+          ? pageSize.height
+          : pageSize.getHeight();
+        doc.text(str, data.settings.margin.left, pageHeight - 10);
+      },
+      margin: { top: 60 },
+      pageBreak: "avoid",
+      rowPageBreak: "auto",
+      bodyStyles: { valign: "top" },
+      styles: {
+        fontSize: 8,
+      },
+    });
+  }
+  // Total page number plugin only available in jspdf v1.0+
+  if (typeof doc.putTotalPages === "function") {
+    doc.putTotalPages(totalPagesExp);
+  }
+  doc.save("curriculo.pdf");
+};
